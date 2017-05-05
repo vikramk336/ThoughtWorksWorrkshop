a30b5ed5db6d5ca3a269cc48c91213920ae59195
Vikram Kulkarni <vikramk336@gmail.com>
2017-05-05 14:09:35 +0530

    first commit

---
+++ bowlinggame/src/test/java/bowlinggame/BowlingGameTest.java
@@ -0,0 +1,47 @@
+package bowlinggame;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+public class BowlingGameTest {  
+private Game g;
+
+@Before
+public void setUp() throws Exception {
+  g = new Game();
+}
+
+
+private void rollMany(int n, int pins) {
+  for (int i = 0; i < n; i++)
+    g.roll(pins);
+}
+@Test
+public void testGutterGame() throws Exception {
+  rollMany(20, 0);
+  assertEquals(0, g.score());
+}
+@Test
+public void testAllOnes() throws Exception {
+  rollMany(20,1);
+  assertEquals(20, g.score());
+}
+@Test
+public void testOneSpare() throws Exception {
+  rollSpare();
+  g.roll(3);
+  rollMany(17,0);
+  assertEquals(16,g.score());
+}
+
+private void rollSpare() {
+  g.roll(5);
+  g.roll(5);
+}
+}
